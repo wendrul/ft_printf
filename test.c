416ea71d12112508c94a8ac692f243bd2364bc39
Wendrul <dethomassin.etienne@gmail.com>
2020-03-10 23:33:24 +0100

    test Kristen

--- test.c
+++ test.c
@@ -5,8 +5,8 @@ int main (int argc, char **argv)
 {
     char *p = "aaaa";
 
-    int a = printf   ("Hello This is a test for -%020c- printf!\n", 'c');
-    int b = ft_printf("Hello This is a test for -%020c- printf!\n", 'c');
+    int a = printf   ("Hello This is a test for -%20p- printf!\n", p);
+    int b = ft_printf("Hello This is a test for -%20p- printf!\n", p);
     printf("difference in returns:  |%d|%d|\n", a, b);
     return (0);
 }
