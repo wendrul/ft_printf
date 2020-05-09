698820b99764b5ba46dbcab1eebb5f5968d31f36
Wendril <dethomassin.etienne@gmail.com>
2020-05-09 17:22:06 +0200

    Make %p as they want it on OSX

--- src/put_numbers.c
+++ src/put_numbers.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 01:09:06 by ede-thom          #+#    #+#             */
-/*   Updated: 2020/05/09 10:34:35 by marvin           ###   ########.fr       */
+/*   Updated: 2020/05/09 17:20:44 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,8 @@ t_buff_manager put_p(t_flag_mod flags, t_buff_manager man, va_list ap)
 {
     unsigned long int        nb;
     void            *ptr;
-    char            p[2 + NB_MAX_WIDTH];
+    char            p[NB_MAX_WIDTH];
+    char            *n;
 
     ptr = va_arg(ap, void*);
     nb = (unsigned long int)ptr;
@@ -105,6 +106,8 @@ t_buff_manager put_p(t_flag_mod flags, t_buff_manager man, va_list ap)
     if (flags.precision == -1)
         flags.precision= va_arg(ap, int);
     itoa_f(p, nb, "0123456789abcdef");
-    return (conversion(man, flags, ztr(p, ft_strlen(p)), ztr("0x", 2)));
-
+    n = p;
+    if (flags.precision == 0)
+        n = "";
+    return (conversion(man, flags, ztr(n, ft_strlen(n)), ztr("0x", 2)));
 }
--- test.c
+++ test.c
@@ -7,8 +7,8 @@ int main (int argc, char **argv)
 
     char *s = "hey \n there is a newline";
     
-    int a = printf   ("p:%09s$\n", "012345", 'o', 'p');
-    int b = ft_printf("f:%09s$\n", "012345", 'o', 'p');
+    int a = printf   ("p:%.p$\n", 3, 'o', 'p');
+    int b = ft_printf("f:%.p$\n", 3, 'o', 'p');
     printf("difference in returns:  |%d|%d|\n", a, b);
     return (0);
 }
